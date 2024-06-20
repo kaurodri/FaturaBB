50db2c6a81b10b4fb73a58d98ac422c10abdec63
HieA1lpha <engkauanrodrigues@gmail.com>
2024-06-20 05:07:38 -0300

    2024-06-20

--- index.js
+++ index.js
@@ -10,11 +10,11 @@ const lerTextoDoPDF = require('./script.js');
     for (let i = 0; i < 5; i++) {
 
         const map = {
-            0 : { regex : /(Página.)([0-9]\/[0-9])/gm, sub : '' },
-            1 : { regex : /(.+[a-z])\:.+[0-9]/g, sub : '' },
-            2 : { regex : /Sao Paulo/g, sub : 'SAO PAULO' },
-            3 : { regex : /Parcela/g, sub : 'PARC' },
-            4 : { regex : /.+[a-z].+/g, sub : '' }
+            0: { regex: /(Página.)([0-9]\/[0-9])/gm, sub: '' },
+            1: { regex: /(.+[a-z])\:.+[0-9]/g, sub: '' },
+            2: { regex: /Sao Paulo/g, sub: 'SAO PAULO' },
+            3: { regex: /Parcela/g, sub: '' },
+            4: { regex: /.+[a-z].+/g, sub: '' }
         }
 
         if (i == 0) {
@@ -30,24 +30,27 @@ const lerTextoDoPDF = require('./script.js');
     let start = ('00/00   ' + lista).split('\n');
     let dados = start.filter(item => item !== ''); //remove os arrays vazios.
 
-    console.log(dados)
-
+    //separar data, conteúdo, preço
     const separarDados = (entrada) => {
         const regex = /^(\d{2}\/\d{2}) (.+) (\d+,\d+)$/;
         const match = entrada.match(regex);
         if (match) {
-            return {
-                data: match[1],
-                conteudo: match[2],
-                valor: match[3]
-            };
+            return [match[1], match[2].replace(/\s+/g, ' ').trim(), match[3]];
         } else {
             return null;
         }
     };
-    
     const dadosSeparados = dados.map(separarDados);
-    
-    console.log(dadosSeparados);
+
+    //ordenar por data.
+    const parseDate = (dateStr) => {
+        const [day, month] = dateStr.split('/').map(Number);
+        return new Date(2024, month - 1, day);
+    };
+    const filteredAndSortedData = dadosSeparados
+        .filter(item => item !== null)
+        .sort((a, b) => parseDate(a[0]) - parseDate(b[0]));
+
+    console.log(filteredAndSortedData);
 
 })();
