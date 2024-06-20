b54ec9b7f3142e2989741fa5a678e3ed199f2345
HieA1lpha <engkauanrodrigues@gmail.com>
2024-06-20 05:36:49 -0300

    2024-06-20

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ const lerTextoDoPDF = require('./script.js');
         const regex = /^(\d{2}\/\d{2}) (.+) (\d+,\d+)$/;
         const match = entrada.match(regex);
         if (match) {
-            return [match[1], match[2].replace(/\s+/g, ' ').trim(), match[3]];
+            return [match[1], match[2].replace(/\s+/g, ' ').trim(), parseFloat(match[3].replace(',', '.'))];
         } else {
             return null;
         }
@@ -52,5 +52,12 @@ const lerTextoDoPDF = require('./script.js');
         .sort((a, b) => parseDate(a[0]) - parseDate(b[0]));
 
     console.log(filteredAndSortedData);
+
+    let valor = 0;
+    for (let i = 0; i < 27; i++) {
+        valor += filteredAndSortedData[i][2];
+    }
+
+    console.log('Valor Total: '+valor.toFixed(2));
 
 })();
